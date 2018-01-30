88effaac94bed5aa363474d43985a2b53250e68d
Bence Kiss <official.bencekiss@gmail.com>
2018-01-30 17:03:14 +0100

    Add filtering in component

--- frontend/src/app/top-scores/components/top-scores/top-scores.component.ts
+++ frontend/src/app/top-scores/components/top-scores/top-scores.component.ts
@@ -14,7 +14,8 @@ export class TopScoresComponent implements OnInit {
   public sortDirection: string;
   public sortBy: string;
   public games: string[];
-  public filterBy: string = 'all';
+  public filter: string = 'all';
+  public filteredTopScores: any[];
 
   constructor(private _topScoresService: TopScoresService) { }
 
@@ -28,6 +29,14 @@ export class TopScoresComponent implements OnInit {
       response => this.games = response,
       error => console.log(error)
     );
+  }
+
+  public filterScores() {
+    if (this.filter != 'all') {
+      this.filteredTopScores = this.topScores.filter(topScore => topScore.game == this.filter);
+    } else {
+      this.filteredTopScores = this.topScores;
+    }
   }
 
   public loadScores(sortBy: string) {
@@ -44,7 +53,10 @@ export class TopScoresComponent implements OnInit {
     }
     this.sortBy = sortBy;
     this._topScoresService.list(this.sortBy, this.sortDirection).subscribe(
-      response => this.topScores = response,
+      response => {
+        this.topScores = response;
+        this.filterScores();
+      },
       error => console.log(error)
     );
   }
