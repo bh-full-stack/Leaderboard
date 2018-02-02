166e17b8687524cc2cac1cb5ce5376edd872d15e
Bence Kiss <official.bencekiss@gmail.com>
2018-02-02 10:21:54 +0100

    Add TinyMCE to registration page

--- frontend/src/app/player/player.module.ts
+++ frontend/src/app/player/player.module.ts
@@ -8,13 +8,15 @@ import { PlayerService } from './services/player.service';
 import { ActivationComponent } from './components/activation/activation.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     PlayerRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ],
   declarations: [RegistrationComponent, ActivationComponent, LoginComponent, ProfileComponent],
   providers: [PlayerService]
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
-import { NavigationComponent } from './components/navigation/navigation.component';
 import { ApiService } from './services/api.service';
 import { AuthService } from './services/auth.service';
+import { NavigationComponent } from './components/navigation/navigation.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { RouterModule } from '@angular/router';
+import { TinyEditorComponent } from './components/tiny-editor/tiny-editor.component';
 
 @NgModule({
   imports: [
@@ -14,10 +15,11 @@ import { RouterModule } from '@angular/router';
     HttpClientModule,
     RouterModule
   ],
-  declarations: [NavigationComponent, NotFoundComponent],
+  declarations: [NavigationComponent, NotFoundComponent, TinyEditorComponent],
   providers: [AuthService],
   exports: [
-    NavigationComponent
+    NavigationComponent,
+    TinyEditorComponent
   ]
 })
 export class SharedModule { }
